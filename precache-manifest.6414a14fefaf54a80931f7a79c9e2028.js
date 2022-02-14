0d88836aa5a02003711eb11f06ab3152a0c5d79d
Shubham Rane <shubhamr022@gmail.com>
2022-02-14 19:30:31 +0530

    Updates

---
+++ precache-manifest.6414a14fefaf54a80931f7a79c9e2028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b025f2daaa049b0f7af9b7a6a4fffa9",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "060584864ba54baf726f",
+    "url": "/profile/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "6d30edccc111a968bb79",
+    "url": "/profile/static/js/2.80ef5dfb.chunk.js"
+  },
+  {
+    "revision": "060584864ba54baf726f",
+    "url": "/profile/static/js/main.2bf156cc.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+  }
+]);
